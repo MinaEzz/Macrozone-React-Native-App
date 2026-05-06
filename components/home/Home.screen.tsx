97bc7946205531c3e407883ee1ee4ceb5573222c
Mina Ezzat <minaezzat98@gmail.com>
2026-05-06 12:40:19 +0300

    feat: cretae notifications utility function - create notification toggle component

--- components/home/Home.screen.tsx
+++ components/home/Home.screen.tsx
@@ -1,11 +1,12 @@
 import { useGetMeals } from "@/hooks/useGetMeals.hook";
 import { globalStyles } from "@/styles/global";
 import { ScrollView, Text, View } from "react-native";
+import CopyButton from "../shared/copy-button/CopyButton.component";
+import ReminderToggle from "../shared/reminder-toggle/ReminderToggle";
 import ShareButton from "../shared/share-button/ShareButton.component";
 import Header from "./header/Header.component";
 import MacroGrid from "./macro-grid/MacroGrid.component";
 import RecentMeals from "./recent-meals/RecentMeals.component";
-import CopyButton from "../shared/copy-button/CopyButton.component";
 
 export default function Home() {
   const { meals, fetchMeals } = useGetMeals();
@@ -20,6 +21,7 @@ export default function Home() {
       <Header />
       <MacroGrid meals={meals} />
       <CopyButton meals={meals} />
+      <ReminderToggle />
       <RecentMeals meals={meals} onDelete={fetchMeals} />
     </ScrollView>
   );
